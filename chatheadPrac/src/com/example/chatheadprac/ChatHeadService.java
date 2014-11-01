24a40c91185caf8566fa4bd87ab7a3aa5c529e41
Elena <hy.kim1122@gmail.com>
2014-11-01 22:22:50 +0900

    ..

---
+++ chatheadPrac/src/com/example/chatheadprac/ChatHeadService.java
@@ -0,0 +1,48 @@
+package com.example.chatheadprac;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class ChatHeadService extends Service {
+
+	private WindowManager windowManager;
+	  private ImageView chatHead;
+
+	  @Override public IBinder onBind(Intent intent) {
+	    // Not used
+	    return null;
+	  }
+
+	  @Override public void onCreate() {
+	    super.onCreate();
+
+	    windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+	    chatHead = new ImageView(this);
+	    chatHead.setImageResource(R.drawable.ic_launcher);
+
+	    WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+	        WindowManager.LayoutParams.WRAP_CONTENT,
+	        WindowManager.LayoutParams.WRAP_CONTENT,
+	        WindowManager.LayoutParams.TYPE_PHONE,
+	        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+	        PixelFormat.TRANSLUCENT);
+
+	    params.gravity = Gravity.TOP | Gravity.LEFT;
+	    params.x = 0;
+	    params.y = 100;
+
+	    windowManager.addView(chatHead, params);
+	  }
+
+	  @Override
+	  public void onDestroy() {
+	    super.onDestroy();
+	    if (chatHead != null) windowManager.removeView(chatHead);
+	  }
+}
